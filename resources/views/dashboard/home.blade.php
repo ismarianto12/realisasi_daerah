579dc841e7195e4182aee60cce66242f4d1862f9
ismaryanto <kotokareh@gmail.com>
2020-10-01 01:28:40 +0700

    update target pendpatan by rian

--- resources/views/dashboard/home.blade.php
+++ resources/views/dashboard/home.blade.php
@@ -24,20 +24,20 @@
         <div class="col-md-6">
             <div class="card full-height">
                 <div class="card-body">
-                    <div class="card-title">Overall statistics</div>
-                    <div class="card-category">Daily information about statistics in system</div>
+                    <div class="card-title">Statistics</div>
+                    <div class="card-category"></div>
                     <div class="d-flex flex-wrap justify-content-around pb-2 pt-4">
                         <div class="px-2 pb-2 pb-md-0 text-center">
                             <div id="circles-1"></div>
-                            <h6 class="fw-bold mt-3 mb-0">New Users</h6>
+                            <h6 class="fw-bold mt-3 mb-0">Jumlah Rek Obj</h6>
                         </div>
                         <div class="px-2 pb-2 pb-md-0 text-center">
                             <div id="circles-2"></div>
-                            <h6 class="fw-bold mt-3 mb-0">Sales</h6>
+                            <h6 class="fw-bold mt-3 mb-0">Jumlah Rek Rincia</h6>
                         </div>
                         <div class="px-2 pb-2 pb-md-0 text-center">
                             <div id="circles-3"></div>
-                            <h6 class="fw-bold mt-3 mb-0">Subscribers</h6>
+                            <h6 class="fw-bold mt-3 mb-0">Jumlah Rek Sub Rincian</h6>
                         </div>
                     </div>
                 </div>
