7e25713cd2f41a015d6eff28e244a2c31fedde03
ismaryanto <kotokareh@gmail.com>
2020-09-30 12:54:28 +0700

    update pendapatan by rian

--- resources/views/settingrek/index.blade.php
+++ resources/views/settingrek/index.blade.php
@@ -2,8 +2,7 @@
 
 @section('title', 'Setting rekening rincian objek per opd')
 @section('content')
-@include('layouts._includes.toolbar')
-
+ 
 <div class="page bg-light">
     <div class="container-fluid my-3">
         <div class="alert alert-danger"> Mendeskripsikan rekening object mana saja yang bisa diakses level dinas .</div>
@@ -13,11 +12,123 @@
                 <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Setting Satker rekening</a>
                 <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">List Rekeing OPD</a>
             </div>
-        </nav>
- 
+        </nav> 
 
         <div class="tab-content" id="nav-tabContent">
             <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="form-row form-inline">
+                            <div class="col-md-12">
+                                <div class="form-group m-0">
+                                    <label for="tmrekening_akun_id" class="col-form-label s-12 col-md-3"><strong>Rek. Akun
+                                            :</strong></label>
+                                    <div class="col-md-5 p-0 mb-2">
+                                        <select name="tmrekening_akun_id" class="form-control r-0 s-12 select2"
+                                            id="tmrekening_akun_id">
+                                            <option value="0">&nbsp;</option>
+                                            @foreach($tmrekening_akuns as $key=>$tmrekening_akun)
+                                            <option value="{{ $tmrekening_akun->id }}">
+                                                {{ '['.$tmrekening_akun->kd_rek_akun.'] '.$tmrekening_akun->nm_rek_akun }}
+                                            </option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                </div>
+                
+                                <div class="form-group m-0">
+                                    <label for="tmrekening_akun_kelompok_id" class="col-form-label s-12 col-md-3"><strong>Rek.
+                                            Kelompok :</strong></label>
+                                    <div class="col-md-5 p-0 mb-2">
+                                        <select name="tmrekening_akun_kelompok_id" class="form-control r-0 s-12 select2"
+                                            id="tmrekening_akun_kelompok_id" onchange="selectOnChange();">
+                                            <option value="0">&nbsp;</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="form-group m-0">
+                                    <label for="tmrekening_akun_kelompok_jenis_id"
+                                        class="col-form-label s-12 col-md-3"><strong>Rek. Jenis</strong></label>
+                                    <div class="col-md-5 p-0 mb-2">
+                                        <select name="tmrekening_akun_kelompok_jenis_id" class="form-control r-0 s-12 select2"
+                                            id="tmrekening_akun_kelompok_jenis_id" onchange="selectOnChange();">
+                                            <option value="0">&nbsp;</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="form-group m-0">
+                                    <label for="tmrekening_akun_kelompok_jenis_objek_id"
+                                        class="col-form-label s-12 col-md-3"><strong>Rek. Obj :</strong></label>
+                                    <div class="col-md-5 p-0 mb-2">
+                                        <select name="tmrekening_akun_kelompok_jenis_objek_id"
+                                            class="form-control r-0 s-12 select2" id="tmrekening_akun_kelompok_jenis_objek_id"
+                                            onchange="selectOnChange();">
+                                            <option value="0">&nbsp;</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <small><i>Jika Level akses OPD = Kosong Silahkan tambahkan Pada rekening rincian dengan memmilih
+                                        list opd di bawah .</i></small>
+                                <div class="form-group m-0">
+                                    <label for="tmrekening_akun_id" class="col-form-label s-12 col-md-3"><strong>Filter By Opd /
+                                            Satker
+                                            :</strong></label>
+                                    <div class="col-md-5 p-0 mb-2">
+                                        <select name="tmsikd_satker_id" id="tmsikd_satker_id" class="form-control select2 "
+                                            required onchange="selectOnChange()">
+                                            <option value="0">Semua Satker (OPD)</option>
+                                            @foreach($tmsikd_satkers as $tmsikd_satker)
+                                            <option value="{{ $tmsikd_satker->id }}">
+                                                [{{ $tmsikd_satker->kode }}] &nbsp; {{ $tmsikd_satker->nama }}
+                                            </option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                </div>
+                                <hr />
+                                <div class="form-group m-0">
+                                    <label for="notsetting" class="col-form-label s-12 col-md-3"><strong>Filter By Belum Di setting
+                                            Satker
+                                            :</strong></label>
+                                    <div class="col-md-5 p-0 mb-2">
+                                        <select name="notsetting" id="notsetting" class="form-control select2 "
+                                            required onchange="selectOnChange()">  
+                                            <option value="0">Semua Object Rincian Setting</option>
+                                            <option value="1">Lihat Yang Belum di Sett</option>  
+                                            <option value="2">Yang Sudah Di Setting</option>
+                                        </select>
+                                    </div>
+                                </div>
+                
+                
+                            </div>
+                        </div>
+
+                        <button class="btn btn-primary"><i class="fa fa-save"></i>Simpan </button>
+                    </div>
+                </div>
+                <div class="card mt-2">
+                    <div class="card-body no-b">
+                        <div id="alert"></div>
+                        <div class="table-responsive">
+                            * ) memeberikan batasan pada setiap opd yang login
+                            <table id="datatable" class="table table-bordered table-striped" style="width:100%">
+                                <thead>
+                                    <th width="30"></th>
+                                    <th width="130">Kode Rek. Rincian Obj</th>
+                                    <th>Nama Rek. Rincian Obj</th>
+                                    <th width="120">Level akses opd</th>
+                                    <th width="120">Klasifikasi</th>
+                                </thead>
+                                <tbody></tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+           
+            <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+                
                 <div class="card">
                     <div class="card-body">
                         <div class="form-row form-inline">
@@ -112,7 +223,7 @@
                         <div id="alert"></div>
                         <div class="table-responsive">
                             * ) memeberikan batasan pada setiap opd yang login
-                            <table id="datatable" class="table table-bordered table-striped" style="width:100%">
+                            <table id="table_dua" class="table table-bordered table-striped" style="width:100%">
                                 <thead>
                                     <th width="30"></th>
                                     <th width="130">Kode Rek. Rincian Obj</th>
@@ -125,10 +236,7 @@
                         </div>
                     </div>
                 </div>
-            </div>
-           
-            <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
-           
+
             </div> 
         </div>
 
@@ -181,6 +289,32 @@
         ]
     });
     //if change data 
+    var table = $('#table_dua').DataTable({
+        processing: true,
+        serverSide: true,
+        order: [1, 'asc'],
+        pageLength: 50,
+        ajax: {
+            url: "{{ route('settingrek.api_data_setting') }}",
+            method: 'POST',
+            data:function(data){
+                data.tmsikd_satker_id   = $('#tmsikd_satker_id').val();
+                data.tmrekening_akun_id = $('#tmrekening_akun_id').val();
+                data.tmrekening_akun_kelompok_id = $('#tmrekening_akun_kelompok_id').val();
+                data.tmrekening_akun_kelompok_jenis_id = $('#tmrekening_akun_kelompok_jenis_id').val();
+                data.tmrekening_akun_kelompok_jenis_objek_id = $('#tmrekening_akun_kelompok_jenis_objek_id').val();
+                data.notsetting   = $('#notsetting').val();
+            }
+        },
+        columns: [
+            {data: 'id', name: 'id', orderable: false, searchable: false, align: 'center', className: 'text-center'},
+            {data: 'kd_rek_rincian_obj', name: 'kd_rek_rincian_obj'},
+            {data: 'nm_rek_rincian_obj', name: 'nm_rek_rincian_obj'},
+            {data: 'nm_satker', name: 'nm_satker',orderable: false, searchable: false},
+            {data: 'klasifikasi_rek', name: 'klasifikasi_rek'},
+        
+        ]
+    });
 
     function selectOnChange()
     {
