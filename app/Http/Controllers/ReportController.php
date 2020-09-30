784f618300601d7c46dc9dcb843e479533da16cc
ismaryanto <kotokareh@gmail.com>
2020-09-30 15:17:55 +0700

    update pendapatan by rian

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -157,6 +157,17 @@ class ReportController extends Controller
     //all data report
     function action_all(Request $request)
     {
+
+        $namaFile = 'Pendapatan_daerah.xls';
+        header("Pragma: public");
+        header("Expires: 0");
+        header("Cache-Control: must-revalidate, post-check=0,pre-check=0");
+        header("Content-Type: application/force-download");
+        header("Content-Type: application/octet-stream");
+        header("Content-Type: application/download");
+        header("Content-Disposition: attachment;filename=" . $namaFile . "");
+        header("Content-Transfer-Encoding: binary "); 
+      
         //dd($request);
         $tahun_id          = $request->tahun_id;
         $tmsikd_satker_id  = $request->tmsikd_satker_id;
