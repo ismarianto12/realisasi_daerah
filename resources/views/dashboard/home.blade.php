7720df20b2ee8dc0094b73e36bd974b6f58359cf
ismaryanto <kotokareh@gmail.com>
2020-10-01 14:50:53 +0700

    update target pendpatan by rian

--- resources/views/dashboard/home.blade.php
+++ resources/views/dashboard/home.blade.php
@@ -84,10 +84,10 @@
 
 @section('script')
 
-<script src="https://code.highcharts.com/highcharts.js"></script>
-<script src="https://code.highcharts.com/modules/exporting.js"></script>
-<script src="https://code.highcharts.com/modules/export-data.js"></script>
-<script src="https://code.highcharts.com/modules/accessibility.js"></script>
+<script src="{{ asset('assets/plugins/hight-cart') }}/highcharts.js"></script>
+<script src="{{ asset('assets/plugins/hight-cart') }}/exporting.js"></script>
+<script src="{{ asset('assets/plugins/hight-cart') }}/export-data.js"></script>
+<script src="{{ asset('assets/plugins/hight-cart') }}/accessibility.js"></script>
 
 <script>
     $(function(){    
