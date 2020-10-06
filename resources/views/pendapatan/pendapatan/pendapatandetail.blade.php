49e12901fcb95270d8719319d7f018b012effd95
ismaryanto <kotokareh@gmail.com>
2020-10-06 13:57:08 +0700

    update form pendaftaran

--- resources/views/pendapatan/pendapatan/pendapatandetail.blade.php
+++ resources/views/pendapatan/pendapatan/pendapatandetail.blade.php
@@ -1,19 +1,18 @@
-<h2>Jenis Pad yang di laporkan  : {{ $data->nm_rek_obj }}</h2>
+<h2>Jenis Pad yang di laporkan : {{ $data->nm_rek_obj }}</h2>
 <h3>Tanggal Lapor {{ $data->tanggal_lapor }}</h3>
 <hr />
-<h4><b>Total dari : {{ $data->nm_rek_obj }} , {{ number_format(0,0,'.',$data->jumlah,'.') }} </b></h4>
+<h4><b>Total dari : {{ $data->nm_rek_obj }} , {{ Html_number::numeric($data->jumlah) }} </b></h4>
 <table class="table table">
 	<tr>
 		<td>Berdasarkan Rek Rincian Object</td>
-		<td>{{ number_format(0,0,'.',$data->jml_rek_rincian_obj,'.') }}</td>
+		<td>{{ Html_number::numeric($data->jml_rek_rincian_obj) }}</td>
 	</tr>
 	<tr>
 		<td>Berdasarkan Rek Object</td>
-		<td>{{ number_format(0,0,'.',$data->jml_rek_obj,'.') }}</td>
+		<td>{{ Html_number::numeric($data->jml_rek_obj) }}</td>
 	</tr>
 	<tr>
 		<td>Berdasarkan Rek Jenis</td>
-		<td>{{ number_format(0,0,'.',$data->jml_rek_jenis,'.') }}</td>
-	</tr> 
+		<td>{{ Html_number::numeric($data->jml_rek_jenis) }}</td>
+	</tr>
 </table>
-  
