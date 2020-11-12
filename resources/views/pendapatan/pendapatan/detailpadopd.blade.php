93f687667c5dc69978fc36d90bb2c0b9ce5a2822
ismaryanto <kotokareh@gmail.com>
2020-11-12 16:31:07 +0700

    update data dari repo

--- resources/views/pendapatan/pendapatan/detailpadopd.blade.php
+++ resources/views/pendapatan/pendapatan/detailpadopd.blade.php
@@ -1,6 +1,11 @@
 <h3>Satuan Kerja / OPD <b>[{{ $satker_kd }}] - {{ $satkernm }}</b></h3>
 <hr />
 
+@if($dataset == '')
+<div class="alert alert-danger"><i class="fa fa-danger"></i> Satker ini belum ada rekening pad </div>
+<img src="https://image.freepik.com/free-vector/error-404-concept-illustration_114360-1811.jpg" class="img-reponsive">
+
+@else
 <table class="table table-striped">
     @foreach ($dataset as $rekeningdatas)
 
@@ -9,10 +14,11 @@
         color: #fff;
         text-align: center;
         font-weight: bold;
-    "; @endif>
+    " ; @endif>
         <td>[{{ $rekeningdatas['kd_rek']['val'] }}] -
             {{ $rekeningdatas['nm_rek']['val'] }}</td>
         <td>@php echo $rekeningdatas['lapor']['val'] @endphp
     </tr>
     @endforeach
 </table>
+@endif
--- storage/framework/views/a43e896634e7c4e82f6eda899760f3c2192c70de.php
+++ storage/framework/views/a43e896634e7c4e82f6eda899760f3c2192c70de.php
@@ -1,6 +1,11 @@
 <h3>Satuan Kerja / OPD <b>[<?php echo e($satker_kd); ?>] - <?php echo e($satkernm); ?></b></h3>
 <hr />
 
+<?php if($dataset == ''): ?>
+<div class="alert alert-danger"><i class="fa fa-danger"></i> Satker ini belum ada rekening pad </div>
+<img src="https://image.freepik.com/free-vector/error-404-concept-illustration_114360-1811.jpg" class="img-reponsive">
+
+<?php else: ?>
 <table class="table table-striped">
     <?php $__currentLoopData = $dataset; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $rekeningdatas): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
 
@@ -9,10 +14,11 @@
         color: #fff;
         text-align: center;
         font-weight: bold;
-    "; <?php endif; ?>>
+    " ; <?php endif; ?>>
         <td>[<?php echo e($rekeningdatas['kd_rek']['val']); ?>] -
             <?php echo e($rekeningdatas['nm_rek']['val']); ?></td>
         <td><?php echo $rekeningdatas['lapor']['val'] ?>
     </tr>
     <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
-</table><?php /**PATH D:\xampp64\www\retribusi\resources\views/pendapatan/pendapatan/detailpadopd.blade.php ENDPATH**/ ?>
+</table>
+<?php endif; ?><?php /**PATH D:\xampp64\www\retribusi\resources\views/pendapatan/pendapatan/detailpadopd.blade.php ENDPATH**/ ?>
