ef0277743f18619811f6972cf5a5bd459766dacd
ismaryanto <kotokareh@gmail.com>
2020-11-19 15:21:10 +0700

    update batangan

--- resources/views/pendapatan/pendapatan/detailpadopd.blade.php
+++ resources/views/pendapatan/pendapatan/detailpadopd.blade.php
@@ -1,46 +1,46 @@
 @if(request()->get('print') == 'y')
-<script>
-    window.print()
+ <script>
+  window.print()
 </script>
 @endif
+<link rel="stylesheet" href="{{ asset('/assets/template/') }}/css/bootstrap.min.css">
 
 <center>
-<h3>Satuan Kerja / OPD <b>[{{ $satker_kd }}] - {{ $satkernm }}</b></h3>
-<hr />
-Pertanggal {{ Properti_app::tgl_indo(date('Y-m-d')) }}
+  <h3>Satuan Kerja / OPD <b>[{{ $satker_kd }}] - {{ $satkernm }}</b></h3>
+  <hr />
+  Pertanggal {{ Properti_app::tgl_indo(date('Y-m-d')) }}
 </center>
 
 @if($dataset == '')
 <div class="alert alert-danger"><i class="fa fa-danger"></i> Satker ini belum ada rekening pad </div>
 <img src="https://image.freepik.com/free-vector/error-404-concept-illustration_114360-1811.jpg" class="img-reponsive">
-   
+
 @else
- @php  
-  $getid = request()->segments(3);
- @endphp
- 
-<a href="{{ Url('pendapatan/dapatkanpadopd/'.$getid[2]) }}?print=y" class="btn btn-primary btn-xs" target="_blank"><i class="fa fa-print"></i>Print
-    Data</a>
+@php
+$getid = request()->segments(3);
+@endphp
+
+<a href="{{ Url('pendapatan/dapatkanpadopd/'.$getid[2]) }}?print=y" class="btn btn-primary btn-xs" target="_blank"><i
+    class="fa fa-print"></i>Print
+  Data</a>
+<hr />
+
 <table class="table table-striped">
-    @foreach ($dataset as $rekeningdatas)
-    <tr @if($rekeningdatas['bold']['val']==TRUE) style="
-        background: green;
-        color: #fff;
+  @foreach ($dataset as $rekeningdatas)
+  <tr @if($rekeningdatas['bold']['val']==TRUE) style="
         text-align: center;
         font-weight: bold;
-    " ; @else 
-    
-     @endif>
-        <td>[{{ $rekeningdatas['kd_rek']['val'] }}] -
-            {{ $rekeningdatas['nm_rek']['val'] }}</td>
-        <td>@php 
-          if($rekeningdatas['lapor']['val'] ==''){
-            echo 'Status Lapor OPD'; 
-          }else{ 
-            echo $rekeningdatas['lapor']['val']; 
-          } 
-          @endphp
-    </tr>
-    @endforeach
+    " ; @else @endif>
+    <td>[{{ $rekeningdatas['kd_rek']['val'] }}] -
+      {{ $rekeningdatas['nm_rek']['val'] }}</td>
+    <td>@php
+      if($rekeningdatas['lapor']['val'] ==''){
+      echo 'Status Lapor OPD';
+      }else{
+      echo $rekeningdatas['lapor']['val'];
+      }
+      @endphp
+  </tr>
+  @endforeach
 </table>
 @endif
--- storage/framework/views/a43e896634e7c4e82f6eda899760f3c2192c70de.php
+++ storage/framework/views/a43e896634e7c4e82f6eda899760f3c2192c70de.php
@@ -1,47 +1,47 @@
 <?php if(request()->get('print') == 'y'): ?>
-<script>
-    window.print()
+ <script>
+  window.print()
 </script>
 <?php endif; ?>
+<link rel="stylesheet" href="<?php echo e(asset('/assets/template/')); ?>/css/bootstrap.min.css">
 
 <center>
-<h3>Satuan Kerja / OPD <b>[<?php echo e($satker_kd); ?>] - <?php echo e($satkernm); ?></b></h3>
-<hr />
-Pertanggal <?php echo e(Properti_app::tgl_indo(date('Y-m-d'))); ?>
+  <h3>Satuan Kerja / OPD <b>[<?php echo e($satker_kd); ?>] - <?php echo e($satkernm); ?></b></h3>
+  <hr />
+  Pertanggal <?php echo e(Properti_app::tgl_indo(date('Y-m-d'))); ?>
 
 </center>
 
 <?php if($dataset == ''): ?>
 <div class="alert alert-danger"><i class="fa fa-danger"></i> Satker ini belum ada rekening pad </div>
 <img src="https://image.freepik.com/free-vector/error-404-concept-illustration_114360-1811.jpg" class="img-reponsive">
-   
+
 <?php else: ?>
- <?php  
-  $getid = request()->segments(3);
- ?>
- 
-<a href="<?php echo e(Url('pendapatan/dapatkanpadopd/'.$getid[2])); ?>?print=y" class="btn btn-primary btn-xs" target="_blank"><i class="fa fa-print"></i>Print
-    Data</a>
+<?php
+$getid = request()->segments(3);
+?>
+
+<a href="<?php echo e(Url('pendapatan/dapatkanpadopd/'.$getid[2])); ?>?print=y" class="btn btn-primary btn-xs" target="_blank"><i
+    class="fa fa-print"></i>Print
+  Data</a>
+<hr />
+
 <table class="table table-striped">
-    <?php $__currentLoopData = $dataset; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $rekeningdatas): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-    <tr <?php if($rekeningdatas['bold']['val']==TRUE): ?> style="
-        background: green;
-        color: #fff;
+  <?php $__currentLoopData = $dataset; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $rekeningdatas): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+  <tr <?php if($rekeningdatas['bold']['val']==TRUE): ?> style="
         text-align: center;
         font-weight: bold;
-    " ; <?php else: ?> 
-    
-     <?php endif; ?>>
-        <td>[<?php echo e($rekeningdatas['kd_rek']['val']); ?>] -
-            <?php echo e($rekeningdatas['nm_rek']['val']); ?></td>
-        <td><?php 
-          if($rekeningdatas['lapor']['val'] ==''){
-            echo 'Status Lapor OPD'; 
-          }else{ 
-            echo $rekeningdatas['lapor']['val']; 
-          } 
-          ?>
-    </tr>
-    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+    " ; <?php else: ?> <?php endif; ?>>
+    <td>[<?php echo e($rekeningdatas['kd_rek']['val']); ?>] -
+      <?php echo e($rekeningdatas['nm_rek']['val']); ?></td>
+    <td><?php
+      if($rekeningdatas['lapor']['val'] ==''){
+      echo 'Status Lapor OPD';
+      }else{
+      echo $rekeningdatas['lapor']['val'];
+      }
+      ?>
+  </tr>
+  <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
 </table>
 <?php endif; ?><?php /**PATH D:\xampp64\www\retribusi\resources\views/pendapatan/pendapatan/detailpadopd.blade.php ENDPATH**/ ?>
